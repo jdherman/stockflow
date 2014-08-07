3b92e18c7eb8c2d4a142abc262ad22440a704b42
jdherman <jdherman8@gmail.com>
2014-08-07 16:41:08 -0400

    odeint backend. new data structures. allow to rerun with different parameters

--- stockflow.py
+++ stockflow.py
@@ -1,42 +1,50 @@
 import numpy as np
-import collections
+from scipy.integrate import odeint
 
 class simulation:
-  def __init__(self, tstep):
-    self.stocks = {}
-    self.flows = collections.OrderedDict()
-    self.tstep = tstep
+  def __init__(self, t):
+    self.t = t
+    self.ix = {}
+    self.flows = {}
+    self.current = []
+    self.done = False
+    self.results = None
 
   def __getattr__(self,key):
-    return self.stocks[key] if key in self.stocks else self.flows[key]['vals']
+    if not self.done: return self.current[self.ix[key]]
+    else: return self.results[:,self.ix[key]]
 
-  def validate_key(self,key):  
-    if key in self.stocks or key in self.flows:
-      raise NameError("Variable " + key + " already defined.")  
+  def __validate_key(self,key):  
+    if key in self.ix: raise NameError("Variable " + key + " already defined.")  
 
-  def init_stocks(self,icdict):
+  def __new_state_var(self, key, IC):
+    self.__validate_key(key)
+    self.current.append(IC)
+    self.ix[key] = len(self.current)-1
+
+  def stocks(self,icdict):
     for k,v in icdict.items():
-      self.stock(k,v)
-
-  def stock(self, key, IC):
-    self.validate_key(key)
-    self.stocks[key] = np.full((self.tstep,), IC) # init time series of stock
+      self.__new_state_var(k,v)
 
   def flow(self, key, f, start=None, end=None):
-    self.validate_key(key)
-    self.flows[key] = {'start': start, 'end': end, 'f': f, 'vals': np.zeros((self.tstep,))}
+    self.__new_state_var(key,f(0))
+    s = self.ix[start] if start is not None else None
+    e = self.ix[end] if end is not None else None
+    self.flows[key] = {'f': f, 'start': s, 'end': e}
+
+  def xdot(self,y,t):
+    self.current = y
+    d = np.zeros((len(y),))
+    for k,f in self.flows.iteritems(): # calculate flows only once. distribute to stocks.
+        i = self.ix[k]
+        ft = f['f'](t)
+        d[i] =  ft - self.current[i]
+        if f['start'] is not None: d[f['start']] -= ft
+        if f['end'] is not None: d[f['end']] += ft
+    return d
 
   def run(self):
-    for t in xrange(1,self.tstep):
-      for stock in self.stocks.itervalues(): # initialize stocks at prior values
-        stock[t] = stock[t-1]
-
-      for flow in self.flows.itervalues(): # calculate flows only once. distribute to stocks.
-        flow['vals'][t-1] = flow['f'](t-1)
-        if flow['start'] is not None:
-          self.stocks[flow['start']][t] -= flow['vals'][t-1]
-        if flow['end'] is not None:
-          self.stocks[flow['end']][t] += flow['vals'][t-1]
-
-    for flow in self.flows.itervalues(): # calculate flows at final timestep
-        flow['vals'][self.tstep-1] = flow['f'](self.tstep-1)
+    self.done = False
+    self.results = odeint(self.xdot, self.current, self.t)
+    self.done = True
+    self.current = self.results[0,:] # restore initial conditions
